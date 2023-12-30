e7e4fe8369bb8438282367b5a57b5ccb9c3275e7
muammerkarasu <muammerkarasu56@gmail.com>
2023-12-30 14:18:00 +0300

    top mechanic

---
+++ soul escape/Assets/Scripts/TopKontrol.cs
@@ -0,0 +1,110 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopKontrol : MonoBehaviour
+{
+    private Rigidbody topRigidbody;
+    private Vector3 baslangicPozisyon;
+    private bool firlatmaModu = false;
+    private Vector3 firlatmaYonu;
+
+    private float _lastMousePositionX, _lastMousePositionY, _deltaX, _deltaY;
+
+    public LayerMask ballLayerMask;
+    public LayerMask wallLayerMask;
+
+    void Start()
+    {
+        topRigidbody = GetComponent<Rigidbody>();
+        baslangicPozisyon = transform.position;
+        topRigidbody.isKinematic = true;
+    }
+
+    void Update()
+    {
+        if (!firlatmaModu && Input.GetMouseButtonDown(0))
+        {
+            _lastMousePositionX = Input.mousePosition.x;
+            _lastMousePositionY = Input.mousePosition.y;
+
+            RaycastAta();
+        }
+
+        if (firlatmaModu)
+        {
+            HareketiKontrolEt();
+        }
+    }
+
+    void RaycastAta()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        Debug.Log("aldi");
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, ballLayerMask))
+        {
+            baslangicPozisyon = transform.position;
+
+            firlatmaModu = true;
+            topRigidbody.isKinematic = false;
+        }
+    }
+
+    void HareketiKontrolEt()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            _deltaX = Input.mousePosition.x - _lastMousePositionX;
+            _deltaY = Input.mousePosition.y - _lastMousePositionY;
+
+            Firlat();
+        }
+    }
+
+    void Firlat()
+    {
+        // Topu fýrlatma yönüne ayarla
+        firlatmaYonu = new Vector3(-_deltaX, -_deltaY, 0f).normalized;
+
+        // Fýrlatma iþlemi
+        firlatmaModu = false;
+        topRigidbody.velocity = firlatmaYonu * speed;
+        topRigidbody.angularVelocity = Vector3.zero;
+    }
+
+    void TopuSifirla()
+    {
+        topRigidbody.velocity = Vector3.zero;
+        topRigidbody.angularVelocity = Vector3.zero;
+        topRigidbody.isKinematic = true;
+        transform.position = baslangicPozisyon;
+    }
+
+    [SerializeField, Range(0f, 250f)] private float speed;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == 7)
+        {
+            WallScript wallScript = collision.gameObject.GetComponent<WallScript>();
+            if (wallScript != null)
+            {
+                firlatmaYonu = Vector3.Reflect(firlatmaYonu, wallScript.normal);
+
+                topRigidbody.velocity = firlatmaYonu * speed;
+                topRigidbody.angularVelocity = Vector3.zero;
+            }
+        }
+        else if (collision.gameObject.layer == 8)
+        {
+            FloorWall wallScript = collision.gameObject.GetComponent<FloorWall>();
+            if (wallScript != null)
+            {
+                topRigidbody.velocity = Vector3.zero;
+                topRigidbody.angularVelocity = Vector3.zero;
+                topRigidbody.isKinematic = true;
+            }
+        }
+    }
+}
